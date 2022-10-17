eb3b942dc6596c02a203019f29b6208966c20d1a
hwangj9642 <hwangjj9642@gmail.com>
2022-10-17 14:12:26 +0900

    Counter test ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
-import Profile from "./Profile";
+// import Profile from "./Profile";
+import Counter from "./Counter";
 
 function App() {
   return (
     <div className="App">
-      <Profile username="JA" name="최진아" />
+      {/* <Profile username="hwangj9642" name="황진주" /> */}
+      <Counter />
       learn react
     </div>
   );
